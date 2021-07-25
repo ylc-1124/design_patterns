4519a6b7d5f4f41b9ff3a9a6bf18783894538608
杨良晨 <853703920@qq.com>
2021-07-25 20:20:14 +0800

    解决反射创建多个实例的问题

--- src/cn/sust/patterns/singleton/demo05/Singleton.java
+++ src/cn/sust/patterns/singleton/demo05/Singleton.java
@@ -4,9 +4,15 @@ package cn.sust.patterns.singleton.demo05;
  * 懒汉式：静态内部类实现（推荐使用）
  */
 public class Singleton {
+    private static boolean flag = false;
     //私有化构造方法
     private Singleton() {
-
+        synchronized (Singleton.class) {
+            if (flag) {
+                throw new RuntimeException("不能创建多个实例对象");
+            }
+            flag = true;
+        }
     }
     //创建一个静态内部类：当外部类被加载时，静态内部类不会被加载
     private static class SingletonHolder {
